d244d353c476b72d15cc7ea709c299e4813ed239
Charles Moog <moogcharlie@gmail.com>
2020-09-23 13:27:14 -0500

    Allow integration tests to pass on fresh deployments (#131)

--- ci/integration/ssh_test.go
+++ ci/integration/ssh_test.go
@@ -5,14 +5,29 @@ import (
 	"testing"
 
 	"cdr.dev/coder-cli/ci/tcli"
+	"cdr.dev/coder-cli/coder-sdk"
 )
 
 func TestSSH(t *testing.T) {
 	t.Parallel()
 	run(t, "ssh-coder-cli-tests", func(t *testing.T, ctx context.Context, c *tcli.ContainerRunner) {
 		headlessLogin(ctx, t, c)
-		c.Run(ctx, "coder config-ssh").Assert(t,
+
+		// TODO remove this once we can create an environment if there aren't any
+		var envs []coder.Environment
+		c.Run(ctx, "coder envs ls --output json").Assert(t,
 			tcli.Success(),
+			tcli.StdoutJSONUnmarshal(&envs),
+		)
+
+		assert := tcli.Success()
+
+		// if we don't have any environments, "coder config-ssh" will fail
+		if len(envs) == 0 {
+			assert = tcli.Error()
+		}
+		c.Run(ctx, "coder config-ssh").Assert(t,
+			assert,
 		)
 	})
 }
