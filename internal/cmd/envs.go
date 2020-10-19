49b57e0656d094691eaf0f6cac30090f0300adb4
Charlie Moog <moogcharlie@gmail.com>
2020-10-19 13:14:23 -0500

    fixup! Allow multiple env args to env stop command

--- internal/cmd/envs.go
+++ internal/cmd/envs.go
@@ -95,6 +95,7 @@ coder envs --user charlie@coder.com ls -o json \
 				egroup.Go(func() error {
 					env, err := findEnv(cmd.Context(), client, envName, *user)
 					if err != nil {
+						flog.Error("failed to find environment by name \"%s\": %v", env.Name, err)
 						return xerrors.Errorf("find environment by name: %w", err)
 					}
 
