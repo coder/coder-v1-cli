ecbc9e13242ce98a70f3b6229af2f05eb51f4c15
Charlie Moog <moogcharlie@gmail.com>
2020-12-03 14:24:39 -0600

    Fix update image tag path

--- coder-sdk/image.go
+++ coder-sdk/image.go
@@ -80,5 +80,5 @@ func (c Client) UpdateImage(ctx context.Context, imageID string, req UpdateImage
 
 // UpdateImageTags refreshes the latest digests for all tags of the image.
 func (c Client) UpdateImageTags(ctx context.Context, imageID string) error {
-	return c.requestBody(ctx, http.MethodPost, "api/images/"+imageID+"/tags/update", nil, nil)
+	return c.requestBody(ctx, http.MethodPost, "/api/images/"+imageID+"/tags/update", nil, nil)
 }
