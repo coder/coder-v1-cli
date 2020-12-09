9411d80d4a9751b9b9f668607018a839b95fa0bf
Charlie Moog <moogcharlie@gmail.com>
2020-12-09 22:47:09 +0000

    Use Makefile for builds and use ubuntu runner

---
+++ Makefile
@@ -0,0 +1,14 @@
+# Makefile for Coder CLI 
+
+.PHONY: clean build/macos build/windows build/linux
+
+clean:
+	rm -rf ./ci/bin
+
+build/macos:
+	CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 ./ci/steps/build.sh
+build/windows:
+	CGO_ENABLED=0 GOOS=windows GOARCH=386 ./ci/steps/build.sh
+build/linux:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 ./ci/steps/build.sh
+
--- ci/steps/build.sh
+++ ci/steps/build.sh
@@ -10,52 +10,38 @@ cd "$(git rev-parse --show-toplevel)/ci/steps"
 
 tag=$(git describe --tags)
 
-build() {
-	echo "--- building coder-cli for $GOOS-$GOARCH"
+echo "--- building coder-cli for $GOOS-$GOARCH"
 
-	tmpdir=$(mktemp -d)
-	go build -ldflags "-X cdr.dev/coder-cli/internal/version.Version=${tag}" -o "$tmpdir/coder" ../../cmd/coder
-	# For MacOS builds to be notarized.
-	cp ../gon.json $tmpdir/gon.json
+tmpdir=$(mktemp -d)
+go build -ldflags "-X cdr.dev/coder-cli/internal/version.Version=${tag}" -o "$tmpdir/coder" ../../cmd/coder
 
-	pushd "$tmpdir"
-	case "$GOOS" in
-		"windows")
-			artifact="coder-cli-$GOOS-$GOARCH.zip"
-			mv coder coder.exe
-			zip "$artifact" coder.exe
-			;;
-		"linux")
-			artifact="coder-cli-$GOOS-$GOARCH.tar.gz"
-			tar -czf "$artifact" coder	
-			;;
-		"darwin")
-		if [[ ${CI-} ]]; then
-			artifact="coder-cli-$GOOS-$GOARCH.zip"
-			gon -log-level debug ./gon.json
-			mv coder.zip $artifact
-		else
-			artifact="coder-cli-$GOOS-$GOARCH.tar.gz"
-			tar -czf "$artifact" coder	
-			echo "--- warning: not in ci, skipping signed release of darwin"
-		fi
-			;;
-	esac
-	popd
+cp ../gon.json $tmpdir/gon.json
 
-	mkdir -p ../bin
-	cp "$tmpdir/$artifact" ../bin/$artifact
-	rm -rf "$tmpdir"
-}
+pushd "$tmpdir"
+case "$GOOS" in
+"windows")
+	artifact="coder-cli-$GOOS-$GOARCH.zip"
+	mv coder coder.exe
+	zip "$artifact" coder.exe
+	;;
+"linux")
+	artifact="coder-cli-$GOOS-$GOARCH.tar.gz"
+	tar -czf "$artifact" coder
+	;;
+"darwin")
+	if [[ ${CI-} ]]; then
+		artifact="coder-cli-$GOOS-$GOARCH.zip"
+		gon -log-level debug ./gon.json
+		mv coder.zip $artifact
+	else
+		artifact="coder-cli-$GOOS-$GOARCH.tar.gz"
+		tar -czf "$artifact" coder
+		echo "--- warning: not in ci, skipping signed release of darwin"
+	fi
+	;;
+esac
+popd
 
-# Darwin builds do not work from Linux, so only try to build them from Darwin.
-# See: https://github.com/cdr/coder-cli/issues/20
-if [[ "$(uname)" == "Darwin" ]]; then
-	CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 build
-else
-	echo "--- warning: Darwin builds don't work on Linux."
-	echo "--- please use an OSX machine to build Darwin tars."
-fi
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 build
-GOOS=windows GOARCH=386 build
+mkdir -p ../bin
+cp "$tmpdir/$artifact" ../bin/$artifact
+rm -rf "$tmpdir"
