c677c8bc3039820d3c56458ece625bf903ddb89b
Charlie Moog <moogcharlie@gmail.com>
2021-01-22 23:54:03 -0600

    fixup! feat: improved arg usage errors

--- internal/x/xcobra/cobra.go
+++ internal/x/xcobra/cobra.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"cdr.dev/coder-cli/pkg/clog"
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +14,7 @@ func ExactArgs(n int) cobra.PositionalArgs {
 		if len(args) != n {
 			return clog.Error(
 				fmt.Sprintf("accepts %d arg(s), received %d", n, len(args)),
-				color.New(color.Bold).Sprintf("usage: ")+cmd.UseLine(),
+				clog.Bold("usage: ")+cmd.UseLine(),
 				clog.BlankLine,
 				clog.Tipf("use \"--help\" for more info"),
 			)
