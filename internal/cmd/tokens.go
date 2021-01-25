1979b04bb9fbd7d1483a42047019379fa06fb3cd
Charlie Moog <moogcharlie@gmail.com>
2021-01-25 00:33:21 +0000

    fixup! chore: cleanup ci scripts

--- internal/cmd/tokens.go
+++ internal/cmd/tokens.go
@@ -13,8 +13,7 @@ import (
 func tokensCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:    "tokens",
-		Short:  "manage Coder API tokens for the active user",
-		Hidden: true,
+			Short:  "manage Coder API tokens for the active user",
 		Long: "Create and manage API Tokens for authenticating the CLI.\n" +
 			"Statically authenticate using the token value with the " + "`" + "CODER_TOKEN" + "`" + " and " + "`" + "CODER_URL" + "`" + " environment variables.",
 	}
