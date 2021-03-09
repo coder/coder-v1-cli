5e36128e0e7021a2d8c0675ca5ad1293199d6bfa
Charlie Moog <moogcharlie@gmail.com>
2021-03-09 12:06:31 -0600

    fixup! feat: simplify login flow

--- internal/cmd/login.go
+++ internal/cmd/login.go
@@ -67,7 +67,7 @@ func login(ctx context.Context, envURL *url.URL) error {
 	authURL.RawQuery = q.Encode()
 
 	if err := browser.OpenURL(authURL.String()); err != nil || true {
-		fmt.Printf("Open the following in your browser:\n\nt%s\n\n", authURL.String())
+		fmt.Printf("Open the following in your browser:\n\n\t%s\n\n", authURL.String())
 	} else {
 		fmt.Printf("Your browser has been opened to visit:\n\n\t%s\n\n", authURL.String())
 	}
