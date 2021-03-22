f0be78baead8ad92fe55342fc526338e37cc868d
Charlie Moog <moogcharlie@gmail.com>
2021-03-22 16:26:52 +0000

    chore: remove dead xterminal code

--- internal/x/xterminal/terminal_windows.go
+++ internal/x/xterminal/terminal_windows.go
@@ -3,10 +3,6 @@
 package xterminal
 
 import (
-	"context"
-	"time"
-
-	"golang.org/x/crypto/ssh/terminal"
 	"golang.org/x/sys/windows"
 )
 
@@ -36,16 +32,6 @@ func makeRaw(handle windows.Handle, input bool) (uint32, error) {
 	return prevState, nil
 }
 
-// MakeRaw sets an input terminal to raw and enables VT100 processing.
-func MakeRaw(handle uintptr) (*State, error) {
-	prevState, err := makeRaw(windows.Handle(handle), true)
-	if err != nil {
-		return nil, err
-	}
-
-	return &State{mode: prevState}, nil
-}
-
 // MakeOutputRaw sets an output terminal to raw and enables VT100 processing.
 func MakeOutputRaw(handle uintptr) (*State, error) {
 	prevState, err := makeRaw(windows.Handle(handle), false)
@@ -59,72 +45,4 @@ func MakeOutputRaw(handle uintptr) (*State, error) {
 // Restore terminal back to original state.
 func Restore(handle uintptr, state *State) error {
 	return windows.SetConsoleMode(windows.Handle(handle), state.mode)
-}
-
-// ColorEnabled returns true if VT100 processing is enabled on the output
-// console.
-func ColorEnabled(handle uintptr) (bool, error) {
-	var state uint32
-	if err := windows.GetConsoleMode(windows.Handle(handle), &state); err != nil {
-		return false, err
-	}
-
-	return state&windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING != 0, nil
-}
-
-// ResizeEvent represent the new size of the terminal.
-type ResizeEvent struct {
-	Height uint16
-	Width  uint16
-}
-
-func (s ResizeEvent) equal(s2 *ResizeEvent) bool {
-	if s2 == nil {
-		return false
-	}
-	return s.Height == s2.Height && s.Width == s2.Width
-}
-
-// ResizeEvents sends terminal resize events when the dimensions change.
-// Windows does not have a unix.SIGWINCH equivalent, so we poll the terminal size
-// at a fixed interval
-func ResizeEvents(ctx context.Context, termFD uintptr) chan ResizeEvent {
-	// Use a buffered chan to avoid blocking if the main is not ready yet when we send the initial resize event.
-	events := make(chan ResizeEvent, 1)
-
-	go func() {
-		defer close(events)
-
-		// On windows, as we don't have a signal to know the size changed, we
-		// use a ticker and emit then event if the current size differs from last time we checked.
-		ticker := time.NewTicker(100 * time.Millisecond)
-		defer ticker.Stop()
-
-		var lastEvent *ResizeEvent
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-ticker.C:
-				width, height, err := terminal.GetSize(int(windows.Handle(termFD)))
-				if err != nil {
-					return
-				}
-				event := ResizeEvent{
-					Height: uint16(height),
-					Width:  uint16(width),
-				}
-				if !event.equal(lastEvent) {
-					select {
-					case <-ctx.Done():
-						return
-					case events <- event:
-					}
-				}
-				lastEvent = &event
-			}
-		}
-	}()
-
-	return events
 }
