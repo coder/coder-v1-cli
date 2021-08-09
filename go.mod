29a48c034be371d8b4ad12375d7f35e48fd0bf40
Kyle Carberry <kyle@coder.com>
2021-08-09 07:48:47 +0000

    feat: Display error to user when SSH'ing into offline workspace

--- go.mod
+++ go.mod
@@ -25,9 +25,10 @@ require (
 	github.com/rjeczalik/notify v0.9.2
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
--- internal/cmd/tunnel.go
+++ internal/cmd/tunnel.go
@@ -12,12 +12,15 @@ import (
 
 	"cdr.dev/slog"
 	"cdr.dev/slog/sloggers/sloghuman"
+	"github.com/fatih/color"
 	"github.com/pion/webrtc/v3"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/xerrors"
 
 	"cdr.dev/coder-cli/coder-sdk"
 	"cdr.dev/coder-cli/internal/x/xcobra"
+	"cdr.dev/coder-cli/pkg/clog"
 	"cdr.dev/coder-cli/wsnet"
 )
 
@@ -59,20 +62,34 @@ coder tunnel my-dev 3000 3000
 			}
 			baseURL := sdk.BaseURL()
 
-			workspaces, err := getWorkspaces(ctx, sdk, coder.Me)
+			workspace, err := findWorkspace(ctx, sdk, args[0], coder.Me)
 			if err != nil {
 				return xerrors.Errorf("get workspaces: %w", err)
 			}
 
-			var workspaceID string
-			for _, workspace := range workspaces {
-				if workspace.Name == args[0] {
-					workspaceID = workspace.ID
-					break
+			if workspace.LatestStat.ContainerStatus != coder.WorkspaceOn {
+				color.NoColor = false
+				notAvailableError := clog.Error("workspace not available",
+					fmt.Sprintf("current status: \"%s\"", workspace.LatestStat.ContainerStatus),
+					clog.BlankLine,
+					clog.Tipf("use \"coder workspaces rebuild %s\" to rebuild this workspace", workspace.Name),
+				)
+				// If we're attempting to forward our remote SSH port,
+				// we want to communicate with the OpenSSH protocol so
+				// SSH clients can properly display output to our users.
+				if remotePort == 12213 {
+					rawKey, err := sdk.SSHKey(ctx)
+					if err != nil {
+						return xerrors.Errorf("get ssh key: %w", err)
+					}
+					err = discardSSHConnection(&stdioConn{}, rawKey.PrivateKey, notAvailableError.String())
+					if err != nil {
+						return err
+					}
+					return nil
 				}
-			}
-			if workspaceID == "" {
-				return xerrors.Errorf("No workspace found by name '%s'", args[0])
+
+				return notAvailableError
 			}
 
 			iceServers, err := sdk.ICEServers(ctx)
@@ -82,14 +99,14 @@ coder tunnel my-dev 3000 3000
 			log.Debug(ctx, "got ICE servers", slog.F("ice", iceServers))
 
 			c := &tunnneler{
-				log:         log,
-				brokerAddr:  &baseURL,
-				token:       sdk.Token(),
-				workspaceID: workspaceID,
-				iceServers:  iceServers,
-				stdio:       args[2] == "stdio",
-				localPort:   uint16(localPort),
-				remotePort:  uint16(remotePort),
+				log:        log,
+				brokerAddr: &baseURL,
+				token:      sdk.Token(),
+				workspace:  workspace,
+				iceServers: iceServers,
+				stdio:      args[2] == "stdio",
+				localPort:  uint16(localPort),
+				remotePort: uint16(remotePort),
 			}
 
 			err = c.start(ctx)
@@ -105,14 +122,14 @@ coder tunnel my-dev 3000 3000
 }
 
 type tunnneler struct {
-	log         slog.Logger
-	brokerAddr  *url.URL
-	token       string
-	workspaceID string
-	iceServers  []webrtc.ICEServer
-	remotePort  uint16
-	localPort   uint16
-	stdio       bool
+	log        slog.Logger
+	brokerAddr *url.URL
+	token      string
+	workspace  *coder.Workspace
+	iceServers []webrtc.ICEServer
+	remotePort uint16
+	localPort  uint16
+	stdio      bool
 }
 
 func (c *tunnneler) start(ctx context.Context) error {
@@ -121,7 +138,7 @@ func (c *tunnneler) start(ctx context.Context) error {
 	dialLog := c.log.Named("wsnet")
 	wd, err := wsnet.DialWebsocket(
 		ctx,
-		wsnet.ConnectEndpoint(c.brokerAddr, c.workspaceID, c.token),
+		wsnet.ConnectEndpoint(c.brokerAddr, c.workspace.ID, c.token),
 		&wsnet.DialOptions{
 			Log:                &dialLog,
 			TURNProxyAuthToken: c.token,
@@ -156,7 +173,7 @@ func (c *tunnneler) start(ctx context.Context) error {
 				return
 			case <-ticker.C:
 				// silently ignore failures so we don't spam the console
-				_ = sdk.UpdateLastConnectionAt(ctx, c.workspaceID)
+				_ = sdk.UpdateLastConnectionAt(ctx, c.workspace.ID)
 			}
 		}
 	}()
@@ -202,4 +219,79 @@ func (c *tunnneler) start(ctx context.Context) error {
 			_, _ = io.Copy(nc, lc)
 		}()
 	}
+}
+
+// Used to treat stdio like a connection for proxying SSH.
+type stdioConn struct{}
+
+func (s *stdioConn) Read(b []byte) (n int, err error) {
+	return os.Stdin.Read(b)
+}
+
+func (s *stdioConn) Write(b []byte) (n int, err error) {
+	return os.Stdout.Write(b)
+}
+
+func (s *stdioConn) Close() error {
+	return nil
+}
+
+func (s *stdioConn) LocalAddr() net.Addr {
+	return nil
+}
+
+func (s *stdioConn) RemoteAddr() net.Addr {
+	return nil
+}
+
+func (s *stdioConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (s *stdioConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (s *stdioConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+// discardSSHConnection accepts a connection then outputs the message provided
+// to any channel opened, immediately closing the connection afterwards.
+//
+// Used to provide status to connecting clients while still aligning with the
+// native SSH protocol.
+func discardSSHConnection(nc net.Conn, privateKey string, msg string) error {
+	config := &ssh.ServerConfig{
+		NoClientAuth: true,
+	}
+	key, err := ssh.ParseRawPrivateKey([]byte(privateKey))
+	if err != nil {
+		return fmt.Errorf("parse private key: %w", err)
+	}
+	signer, err := ssh.NewSignerFromKey(key)
+	if err != nil {
+		return fmt.Errorf("signer from private key: %w", err)
+	}
+	config.AddHostKey(signer)
+	conn, chans, reqs, err := ssh.NewServerConn(nc, config)
+	if err != nil {
+		return fmt.Errorf("create server conn: %w", err)
+	}
+	go ssh.DiscardRequests(reqs)
+	ch, req, err := (<-chans).Accept()
+	if err != nil {
+		return fmt.Errorf("accept channel: %w", err)
+	}
+	go ssh.DiscardRequests(req)
+
+	_, err = ch.Write([]byte(msg))
+	if err != nil {
+		return fmt.Errorf("write channel: %w", err)
+	}
+	err = ch.Close()
+	if err != nil {
+		return fmt.Errorf("close channel: %w", err)
+	}
+	return conn.Close()
 }
