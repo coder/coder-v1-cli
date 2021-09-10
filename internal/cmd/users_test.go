3d820e422ed1e37f65716c3c88ba6c1c591489e2
Cian Johnston <cian@coder.com>
2021-09-10 10:48:05 +0100

    add missing cherry-pick: chore: Use webrtc for coder sh (#408) (#435)
    * chore: Use webrtc for coder sh (#408)
    - Use webrtc tunnel for `coder sh` command
    * chore(cli): "fix" broken unit test
    
    Co-authored-by: Steven Masley <Emyrk@users.noreply.github.com>

--- internal/cmd/users_test.go
+++ internal/cmd/users_test.go
@@ -3,9 +3,6 @@ package cmd
 import (
 	"testing"
 
-	"cdr.dev/slog"
-	"cdr.dev/slog/sloggers/slogtest"
-
 	"cdr.dev/coder-cli/coder-sdk"
 )
 
@@ -16,17 +13,7 @@ func Test_users(t *testing.T) {
 	res := execute(t, nil, "users", "ls", "--output=json")
 	res.success(t)
 	res.stdoutUnmarshals(t, &users)
-	assertAdmin(t, users)
 
 	res = execute(t, nil, "users", "ls", "--output=human")
 	res.success(t)
-}
-
-func assertAdmin(t *testing.T, users []coder.User) {
-	for _, u := range users {
-		if u.Username == "admin" {
-			return
-		}
-	}
-	slogtest.Fatal(t, "did not find admin user", slog.F("users", users))
 }
